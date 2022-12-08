34ba86edfb5a92af13bb4de84ee83c8139600eba
Davide Ghilardi <ghilardi.davide@gmail.com>
2022-12-09 00:53:51 +0100

    add path as separate field in configuration

--- src/project/config.rs
+++ src/project/config.rs
@@ -7,7 +7,7 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 pub struct PanProjectConfig {
     vcs: VcsConfig,
-    modules: HashMap<PathBuf, ProjectModule>,
+    modules: HashMap<String, ProjectModule>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -17,8 +17,15 @@ pub enum VcsConfig {
 }
 
 #[derive(Deserialize, Debug)]
+pub struct ProjectModule {
+    path: PathBuf,
+    #[serde(flatten)]
+    package_manager: PackageManager,
+}
+
+#[derive(Deserialize, Debug)]
 #[serde(tag = "packageManager")]
-pub enum ProjectModule {
+pub enum PackageManager {
     Cargo,
     Npm,
     Maven,
@@ -28,33 +35,36 @@ impl PanProjectConfig {
     pub fn load(path: &Path) -> anyhow::Result<PanProjectConfig> {
         let conf_str = fs::read_to_string(path.join(".panproject.toml"))
             .with_context(|| format!("Failed to read .panproject.toml from {:?}", path))?;
-        let conf: PanProjectConfig = toml::from_str(&conf_str)?;
+        let mut conf: PanProjectConfig = toml::from_str(&conf_str)?;
 
-        conf.modules.iter()
-            .map(|(mod_path, conf)| Self::validate_module(path.join(mod_path), conf))
+        conf.modules.iter_mut()
+            .map(|(mod_name, conf)| {
+                conf.path = path.join(&conf.path);
+                Self::validate_module(mod_name, conf)
+            })
             .collect::<Result<Vec<()>, _>>()?;
 
         Ok(conf)
     }
 
-    fn validate_module(path: PathBuf, module_conf: &ProjectModule) -> anyhow::Result<()> {
-        match module_conf {
-            ProjectModule::Cargo => {
-                let cargo_toml_path = path.join("Cargo.toml");
+    fn validate_module(mod_name: &str, module_conf: &ProjectModule) -> anyhow::Result<()> {
+        match module_conf.package_manager {
+            PackageManager::Cargo => {
+                let cargo_toml_path = module_conf.path.join("Cargo.toml");
                 if !cargo_toml_path.is_file() {
-                    return Err(anyhow!("{:?} is not a valid file", cargo_toml_path));
+                    return Err(anyhow!("Error during {mod_name} module validation. {:?} is not a valid file", cargo_toml_path));
                 }
             }
-            ProjectModule::Npm => {
-                let cargo_toml_path = path.join("package.json");
+            PackageManager::Npm => {
+                let cargo_toml_path = module_conf.path.join("package.json");
                 if !cargo_toml_path.is_file() {
-                    return Err(anyhow!("{:?} is not a valid file", cargo_toml_path));
+                    return Err(anyhow!("Error during {mod_name} module validation. {:?} is not a valid file", cargo_toml_path));
                 }
             }
-            ProjectModule::Maven => {
-                let cargo_toml_path = path.join("pom.xml");
+            PackageManager::Maven => {
+                let cargo_toml_path = module_conf.path.join("pom.xml");
                 if !cargo_toml_path.is_file() {
-                    return Err(anyhow!("{:?} is not a valid file", cargo_toml_path));
+                    return Err(anyhow!("Error during {mod_name} module validation. {:?} is not a valid file", cargo_toml_path));
                 }
             }
         }
