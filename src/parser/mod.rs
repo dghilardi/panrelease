4587c7c68849391e1de4b71829d13ff1030ff219
Davide Ghilardi <ghilardi.davide@gmail.com>
2022-12-18 14:42:57 +0100

    implement json parser (from nom example)

---
+++ src/parser/mod.rs
@@ -0,0 +1,6 @@
+pub mod json;
+
+pub trait FormatCodec {
+    fn extract(&self, path: &str) -> anyhow::Result<Option<&str>>;
+    fn replace(&mut self, path: &str, value: &str) -> anyhow::Result<bool>;
+}
